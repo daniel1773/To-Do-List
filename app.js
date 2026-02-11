2ea1412a785292656fcc9c55c54dab28179a689e
Daniel Alves <danalvessalesferreira@gmail.com>
2026-02-10 22:44:23 -0300

    Melhora JS, prioridades com cores, letras iniciais maiusculas

--- app.js
+++ app.js
@@ -3,21 +3,26 @@ const ul_tarefas = document.getElementById("tarefas");
 const nome_tarefa = document.getElementById("nome_tarefa");
 const prioridade_tarefa = document.getElementsByName("prioridade"); //Retorna uma lista
 
-
-function add_tarefa(){
-    let prioridadeSelecionada = "";
+function add_tarefa(){  
+    const span_prioridade = document.createElement("span");
+    let prioridade_selecionada = "";
     
     for (let radio of prioridade_tarefa){
         if(radio.checked){
-            prioridadeSelecionada = radio.value;
+            prioridade_selecionada = radio.value;
             break;
         }
     }
+    span_prioridade.textContent = prioridade_selecionada;
+    span_prioridade.classList.add(prioridade_selecionada);
 
-    if(nome_tarefa.value === "" || prioridadeSelecionada === ""){
+    if(nome_tarefa.value === "" || prioridade_selecionada === ""){
         return error.innerText = "Por favor, preencha todos os campos!";
     }
-
     error.innerText = ""; //Limpa a mensagem de erro
-    ul_tarefas.innerHTML += `<li>${nome_tarefa.value} | ${prioridadeSelecionada}</li>`;
+    
+    const li = document.createElement("li");
+    li.textContent = `${nome_tarefa.value} | `;
+    li.appendChild(span_prioridade);
+    ul_tarefas.appendChild(li);
 }
