a3bfe9c18696927a62cbf29b71e45b1c11aee7b0
Daniel Alves <danalvessalesferreira@gmail.com>
2026-03-17 17:18:25 -0300

    adicionado ferramenta de excluir tarefas

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-// Adicionando novas tarefas
+// Adicionando novas variáveis
 const error = document.getElementById("error");
 const ul_tarefas = document.getElementById("tarefas");
 const nome_tarefa = document.getElementById("nome_tarefa");
 const prioridade_tarefa = document.getElementsByName("prioridade"); //Retorna uma lista
 
+// Adicionando tarefas
 function add_tarefa(){  
     const span_prioridade = document.createElement("span");
     let prioridade_selecionada = "";
@@ -14,16 +15,19 @@ function add_tarefa(){
             break;
         }
     }
-    span_prioridade.textContent = prioridade_selecionada;
+    span_prioridade.textContent = `  | ${prioridade_selecionada}`;
     span_prioridade.classList.add(prioridade_selecionada);
 
     if(nome_tarefa.value === "" || prioridade_selecionada === ""){
         return error.innerText = "Por favor, preencha todos os campos!";
     }
     error.innerText = ""; //Limpa a mensagem de erro
+
+    const span_nome_tarefa = document.createElement("span");
+    span_nome_tarefa.textContent = nome_tarefa.value.slice(0, 1).toUpperCase() + nome_tarefa.value.slice(1);
     
     const li = document.createElement("li");
-    li.textContent = `${nome_tarefa.value} | `;
+    li.appendChild(span_nome_tarefa);
     li.appendChild(span_prioridade);
     ul_tarefas.appendChild(li);
 }
@@ -38,21 +42,20 @@ btn_tema.addEventListener("click", ()=>{
 
 
 // Excluindo tarefas
-function ativar_excluir_tarefas(){
+function remove_tarefa(){
     ul_tarefas.classList.toggle("sem-marcador")
 
     const li_tarefas = document.querySelectorAll("#tarefas li"); //seleciona todos os "li" da lista "tarefas"
-    const btn_X = document.querySelectorAll(".btn-delete");
 
-    if(btn_X.length === 0){
-        li_tarefas.forEach((li)=>{
-            const btn_delete = document.createElement("span");
-            btn_delete.textContent = "X";
-            btn_delete.classList.add("btn-delete");
+    li_tarefas.forEach((li)=>{
+        const btn_delete = document.createElement("button");
+        btn_delete.textContent = "❌";
+        btn_delete.classList.add("btn-delete");
 
-            li.prepend(btn_delete);
-        })
-    } else {
-        btn_X.forEach((X)=> X.remove())
-    }
+        btn_delete.onclick = ()=>{
+            li.remove();
+        }
+
+        li.appendChild(btn_delete);
+    })
 }
